3445121c1893dbf1c5389e9f9308b3d75bc7e458
Sofia Meroni <58039400+SofiaMeroni@users.noreply.github.com>
2024-11-08 12:49:24 -0300

    Agregar archivos iniciales para pipeline

--- importar_datos.sql
+++
@@ -1,3 +0,0 @@
-\copy condiciones(poda) FROM 'C:/Users/smero/OneDrive/Documentos/repos/Cuidador-de-plantas/poda.csv' DELIMITER ',' CSV HEADER;
-\copy condiciones(humedad) FROM 'C:/Users/smero/OneDrive/Documentos/repos/Cuidador-de-plantas/humedad.csv' DELIMITER ',' CSV HEADER;
-\copy condiciones(sol_directo) FROM 'C:/Users/smero/OneDrive/Documentos/repos/Cuidador-de-plantas/sol_directo.csv' DELIMITER ',' CSV HEADER;
